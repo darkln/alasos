dbae9634f8a7c3637ac7f1dad00fef0dd7ba959a
parker <31112164+darkln@users.noreply.github.com>
2023-02-16 11:03:56 -0500

    main

---
+++ alasos/Pages/Register.cshtml.cs
@@ -0,0 +1,17 @@
+using alasos.Properties;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Parse;
+using System.Resources;
+
+namespace alasos.Pages
+{
+    public class RegisterModel : PageModel
+    {
+
+        public void OnGet()
+        {
+        }
+
+    }
+}
