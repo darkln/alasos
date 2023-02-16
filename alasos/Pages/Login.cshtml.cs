859a403236accebf0d7eafa2a6621ebfbdeb9686
parker <31112164+darkln@users.noreply.github.com>
2023-02-16 15:10:37 -0500

    BIG

--- alasos/Pages/Login.cshtml.cs
+++ alasos/Pages/Login.cshtml.cs
@@ -1,17 +1,12 @@
-using alasos.Properties;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Parse;
-using System.Resources;
 
 namespace alasos.Pages
 {
-    public class RegisterModel : PageModel
+    public class LoginModel : PageModel
     {
-
         public void OnGet()
         {
         }
-
     }
 }
